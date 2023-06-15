c1491eff2dfbbbef88d77a052dcdf66dc9f33159
Ferdinand Estoque <festoqufx@gmail.com>
2023-06-15 15:47:05 +0800

    zxczcx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: 'center' }}>
+      All &copy; &nbsp;|&nbsp;FERDINAND BELLEZA ESTOQUE&nbsp;|&nbsp;All Rights Reserved
+      <p style={{ marginTop: '2px' }}>
+        Follow Raven on{' '}
+        <a href="https://www.linkedin.com/in/ferdinand-estoque-46797876/">LinkedIn</a> and{' '}
+        <a href="https://github.com/festoqufx">GitHub</a> for more.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
